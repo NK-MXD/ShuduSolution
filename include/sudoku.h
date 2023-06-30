1e57426008a0c4a789b2ff9d74d97e47224549b4
xiaoduo <xd19834739907@163.com>
2023-07-01 01:05:28 +0800

    modify makefile and readme

---
+++ include/sudoku.h
@@ -0,0 +1,33 @@
+
+#ifndef SRC_SUDOKU_H_
+#define SRC_SUDOKU_H_
+
+#include <array>
+#include <string>
+#include <vector>
+
+// 定义数独棋盘数据结构
+typedef std::array<std::array<int, 9>, 9> SudokuBoard;
+// 打印数独棋盘
+void printSudokuBoard(const SudokuBoard &board);
+// 从文件中读取数独棋盘
+std::vector<SudokuBoard> readSudokuBoardFromFile(const std::string &filename);
+// 将数独棋盘写入文件
+void writeSudokuBoardToFile(const std::string &filename, const std::vector<SudokuBoard> &boards);
+// 生成数独游戏
+std::vector<SudokuBoard> generateGameSudoku(int count, int mode, std::pair<int, int> range, bool unique);
+// 解决数独游戏
+SudokuBoard solveSudoku(SudokuBoard board);
+// 根据基础数独终局生成所有不重复的数独终局
+std::vector<SudokuBoard> generateAllSudokuBoards(int numSolutions);
+// 生成数独游戏
+std::vector<SudokuBoard> generateGameSudoku(int count, int mode, std::pair<int, int> range, bool unique);
+// 生成数独游戏实现
+SudokuBoard generateGameSudokuImpl(const SudokuBoard &finalBoard, int mode, std::pair<int, int> range, bool unique);
+// 挖空数独终局生成数独
+SudokuBoard digBoard(SudokuBoard finalBoard, int digNum);
+// 求解所有解的个数
+void solveSudoku(int i, int j, SudokuBoard &board, int solutionCount);
+int genRand();
+
+#endif
